7b1d98d8b968b2e593f7f662d7cbd555c5641723
RISHABH MISHRA <rishabhmishra.81e@gmail.com>
2024-06-24 11:26:29 +0530

    back to normal

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -13,15 +13,11 @@ const Services = () => {
         <div>Our Services</div>
       </div>
       <div className="services__holder">
-        <div className="service__item">
-          <img
-            src="./scholarship.png"
-            alt="Scholarship"
-            className="service__image"
-          />
-          <div className="service__content">
-            <div className="service__heading">EDUCATIONAL SERVICES</div>
-            <div className="service__description">
+        <div className="education__service__container">
+          <img src="./scholarship.png" style={{ height: "100px" }} />
+          <div className="education__ser__content">
+            <div className="edu__ser__heading">EDUCATIONAL SERVICES</div>
+            <div className="edu__ser__des">
               The Career Zone started in 2001 to help students find the right
               schools. Today, they are number one in educational consulting and
               work with many colleges and universities in India (including Uttar
@@ -29,22 +25,17 @@ const Services = () => {
               Rajasthan, Tamil Nadu) and even overseas.
             </div>
             <Link to={"/services/edu_service"}>
-              <div className="service__btn" onClick={() => scrollTop()}>
+              <div className="edu__btn" onClick={() => scrollTop()}>
                 Read More
               </div>
             </Link>
           </div>
         </div>
-        <br />
-        <div className="service__item">
-          <img
-            src="./credit.png"
-            alt="Credit Card"
-            className="service__image"
-          />
-          <div className="service__content">
-            <div className="service__heading">STUDENT CREDIT CARD</div>
-            <div className="service__description">
+        <div className="education__service__container">
+          <img src="./credit.png" style={{ height: "100px" }} />
+          <div className="education__ser__content">
+            <div className="edu__ser__heading">STUDENT CREDIT CARD</div>
+            <div className="edu__ser__des">
               The Bihar Student Credit Card Yojana, also called "MNSSBY Bihar
               Student Credit Card Scheme", is a program that started in October
               2016. This program helps students by giving them loans to pay for
@@ -52,7 +43,7 @@ const Services = () => {
               you can now check the status of your application.
             </div>
             <Link to={"/services/stdCred_service"}>
-              <div className="service__btn" onClick={() => scrollTop()}>
+              <div className="edu__btn" onClick={() => scrollTop()}>
                 Read More
               </div>
             </Link>
