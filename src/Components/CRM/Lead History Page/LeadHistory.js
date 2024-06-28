744074453b14983ff0b1af43c1b890744d4a9dfb
RISHABH MISHRA <rishabhmishra.81e@gmail.com>
2024-06-28 18:47:52 +0530

    chore: Refactor CreateNewLead component and update home icon in CreateNewLead module

--- src/Components/CRM/Lead History Page/LeadHistory.js
+++ src/Components/CRM/Lead History Page/LeadHistory.js
@@ -6,10 +6,9 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
 import PopUpForm from "./PopUpForm/PopUpForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-regular-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faTrash, faPenSquare } from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
+
 const LeadHistory = () => {
   const [leads, setLeads] = useState([]);
   const navigate = useNavigate();
@@ -22,6 +21,10 @@ const LeadHistory = () => {
     assignmentDate: "",
     assignedBy: "",
   });
+
+  const goToHomePage = () => {
+    navigate("/dashboard");
+  };
 
   const columns = [
     { field: "serialNumber", headerName: "S.no", sortable: false },
@@ -49,10 +52,10 @@ const LeadHistory = () => {
           variant="contained"
           onClick={() => handleView(params.row.id)}
           sx={{
-            backgroundColor: "#03C03C", // Set your desired background color
-            color: "#FFFFFF", // Set your desired text color
-            paddingTop: "10px", // Add padding on top
-            paddingBottom: "10px", // Add padding on bottom
+            backgroundColor: "#03C03C",
+            color: "#FFFFFF",
+            paddingTop: "10px",
+            paddingBottom: "10px",
             "&:hover": { backgroundColor: "green" },
           }}
         >
@@ -69,10 +72,10 @@ const LeadHistory = () => {
           variant="contained"
           onClick={() => handleDelete(params.row.id)}
           sx={{
-            backgroundColor: "#FF0800", // Set your desired background color
-            color: "#FFFFFF", // Set your desired text color
-            paddingTop: "10px", // Add padding on top
-            paddingBottom: "10px", // Add padding on bottom
+            backgroundColor: "#FF0800",
+            color: "#FFFFFF",
+            paddingTop: "10px",
+            paddingBottom: "10px",
             "&:hover": { backgroundColor: "red" },
           }}
         >
@@ -89,10 +92,10 @@ const LeadHistory = () => {
           variant="contained"
           onClick={() => handleUpdate(params.row.id)}
           sx={{
-            backgroundColor: "dodgerblue", // Set your desired background color
-            color: "#FFFFFF", // Set your desired text color
-            paddingTop: "10px", // Add padding on top
-            paddingBottom: "10px", // Add padding on bottom
+            backgroundColor: "dodgerblue",
+            color: "#FFFFFF",
+            paddingTop: "10px",
+            paddingBottom: "10px",
             "&:hover": { backgroundColor: "dodgerblue" },
           }}
         >
@@ -158,14 +161,13 @@ const LeadHistory = () => {
     });
 
     if (confirmDelete.isConfirmed) {
-      // Perform lead deletion logic here
       await db.collection("leads").doc(id).delete();
       Swal.fire("Deleted!", "", "success");
     } else {
-      // Display a message indicating cancellation
       Swal.fire("Cancelled", "", "info");
     }
   };
+
   const handleUpdate = (id) => {
     setUpdate(true);
     leads.forEach((data) => {
@@ -198,18 +200,21 @@ const LeadHistory = () => {
       </div>
       <div style={{ position: "relative" }}>
         <div className={styles["heading-with-homebtn"]}>
-          <div>
-            <a href="/dashboard">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="60"
-                fill="black"
-                class="bi bi-house"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
-              </svg>
-            </a>
+          <div
+            onClick={goToHomePage}
+            style={{ cursor: "pointer" }}
+            className="mt-10"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="56"
+              fill="currentColor"
+              class="bi bi-house-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+              <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+            </svg>
           </div>
           <div className={styles.heading}>Lead History</div>
         </div>
